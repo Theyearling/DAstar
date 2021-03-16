83e136c529eebc21032a981d1a0b998a97b8c458
Theyearling <49585691+Theyearling@users.noreply.github.com>
2021-03-16 15:22:47 +0800

    Update DAstar.cpp

--- my_globalplanner/src/DAstar.cpp
+++ my_globalplanner/src/DAstar.cpp
@@ -1,13 +1,14 @@
 #include <DAstar.h>
 #include <pluginlib/class_list_macros.h>
 
+
 // register as a plugin
-PLUGINLIB_EXPORT_CLASS(my_globalplanner::DAstar, nav_core::BaseGlobalPlanner)
+PLUGINLIB_EXPORT_CLASS(my_planner::DAstar, nav_core::BaseGlobalPlanner)
 
-namespace my_globalplanner{
-    DAstar::DAstar() : lethal_cost_(253), costmap_ros_(NULL), initialized_(false) {};
+namespace my_planner{
+    DAstar::DAstar(){};
 
-    DAstar::DAstar(std::string name, costmap_2d::Costmap2DROS* costmap_ros) : lethal_cost_(253), costmap_ros_(NULL), initialized_(false) {
+    DAstar::DAstar(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
         initialize(name, costmap_ros); 
     }
 
@@ -19,13 +20,15 @@ namespace my_globalplanner{
             height = costmap_->getSizeInCellsY();
             map_size = width * height;
             costs = costmap_->getCharMap();
-          
+            
+
             frame_id_ = costmap_ros->getGlobalFrameID();
             ros::NodeHandle private_nh("~/" + name);
+
             plan_pub_ = private_nh.advertise<nav_msgs::Path>("plan", 1);
+
             initialized_ = true;
         }
-
         else
         {
             ROS_WARN("This planner has already been initialized.. doing noting");
@@ -39,10 +42,9 @@ namespace my_globalplanner{
             ROS_ERROR("The plannner has not been initialized...");
             return false;
             }
-        
-        ROS_INFO("Got a start: %.2f, %.2f, and a goal: %.2f, %.2f", start.pose.position.x, start.pose.position.y,
+        ROS_INFO("Got a start: %.2f, %.2f, and a goal: %.2f, %.2f", start.pose.position.x, start.pose.position.y, 
             goal.pose.position.x, goal.pose.position.y);
-       
+        
         // the coordinates of the starting point in the world
         double wx = start.pose.position.x;
         double wy = start.pose.position.y;
@@ -82,19 +84,18 @@ namespace my_globalplanner{
         // to store the parent point of the current point
         std::vector<int> cameFrom(map_size, -1);
         std::vector<int> decameFrom(map_size, -1);
-        plan.clear();
 
+        plan.clear();
+        
         // Loop, search grids in the map
         while(queue_.size() > 0 && dequeue_.size() > 0){
             Node top = queue_[0], detop = dequeue_[0]; // get the minimum cost of point
             std::pop_heap(queue_.begin(), queue_.end(), greater1());
             std::pop_heap(dequeue_.begin(), dequeue_.end(), greater1());
-
             queue_.pop_back();
             dequeue_.pop_back();
 
             int i = top.index, de_i = detop.index;
-
             if(gCost[de_i] < MAX_INIT){ 
                 // in the positive direction 
                 // searched the grid that has been traveled in the opposite direction
@@ -115,7 +116,7 @@ namespace my_globalplanner{
                 return true;
             }
 
-            if(de_i = start_index){
+            if(de_i == start_index){
                 // in the opposite direction, reach the ending
                 getPlan(cameFrom, decameFrom, start_index, goal_index, plan);
                 return true;
@@ -125,25 +126,28 @@ namespace my_globalplanner{
 
             // search grid in the opposite direction
             addNeigbors(decameFrom, degCost, de_i, start_index, 0);
+
         }
         return false;
     }
 
     void DAstar::getPlan1(std::vector<int> camefrom, std::vector<int> decamefrom, int start_i, int goal_i,
             std::vector<geometry_msgs::PoseStamped>& plan, int index){
+        
         std::vector<int> camePath;
         int came = index, decame = index;
-
         while(came != start_i){
+
             //get indexes of that path has been finded, positive direction
             camePath.push_back(camefrom[came]);
             came = camefrom[came];
         }
-        
+
         std::reverse(camePath.begin(), camePath.end());
         camePath.push_back(index); // add the coincident index
 
         while(decame != goal_i){
+
             // get indexes of that path has been finded, opposite direction
             camePath.push_back(decamefrom[decame]);
             decame = decamefrom[decame];
@@ -151,15 +155,14 @@ namespace my_globalplanner{
 
         plan.clear();
         ros::Time plan_time = ros::Time::now();
-
         for(int i = 0; i < camePath.size(); i++){
             unsigned mx, my;
             costmap_->indexToCells(camePath[i], mx, my);
             double wx, wy;
             costmap_->mapToWorld(mx, my, wx, wy);
+
             geometry_msgs::PoseStamped pose;
             pose.header.stamp = plan_time;
-
             pose.header.frame_id = costmap_ros_->getGlobalFrameID();
             pose.pose.position.x = wx;
             pose.pose.position.y = wy;
@@ -173,10 +176,12 @@ namespace my_globalplanner{
             plan.push_back(pose);
         }
         publishPlan(plan);
+
     }
 
     void DAstar::getPlan(std::vector<int> camefrom, std::vector<int> decamefrom, int start_i, int goal_i,
             std::vector<geometry_msgs::PoseStamped>& plan){
+
         std::vector<int> camePath;
 
         // in positive direction, if it has not reached the ending
@@ -190,29 +195,27 @@ namespace my_globalplanner{
                 came = decamefrom[came];
             } 
         }
-
         else{
             int came = goal_i;
             while(came != start_i){
+
                 // get indexes of that path has been finded, opposite direction
                 camePath.push_back(camefrom[came]);
                 came = camefrom[came];
-
             }
             std::reverse(camePath.begin(), camePath.end());
             // add the index of goal point
             camePath.push_back(goal_i);
-
         }
 
         plan.clear();
         ros::Time plan_time = ros::Time::now();
-
         for(int i = 0; i < camePath.size(); i++){
             unsigned mx, my;
             costmap_->indexToCells(camePath[i], mx, my);
             double wx, wy;
             costmap_->mapToWorld(mx, my, wx, wy);
+
             geometry_msgs::PoseStamped pose;
             pose.header.stamp = plan_time;
             pose.header.frame_id = costmap_ros_->getGlobalFrameID();
@@ -220,27 +223,30 @@ namespace my_globalplanner{
             pose.pose.position.x = wx;
             pose.pose.position.y = wy;
             pose.pose.position.z = 0.0;
-            
+
             pose.pose.orientation.x = 0.0;
             pose.pose.orientation.y = 0.0;
             pose.pose.orientation.z = 0.0;
             pose.pose.orientation.w = 1.0;
-       
+
             plan.push_back(pose);
         }
         publishPlan(plan);
+
     }
 
     void DAstar::addNeigbors(std::vector<int>& camefrom, std::vector<float>& gcost, int i, int goal_i, int judge){
+
         for(int x = -1; x <= 1; x++){
             for(int y = -1; y <= 1; y++){
+
                 if(x == 0 && y == 0) continue;
 
                 unsigned xi, yi;
                 costmap_->indexToCells(i, xi, yi);
                 int next_x = xi + x;
                 int next_y = yi + y;
-                int next_i = costmap_->getIndex(xi, yi);
+                int next_i = costmap_->getIndex(next_x, next_y);
 
                 // not in map
                 if(next_i < 0 || next_i > map_size) continue;
@@ -249,7 +255,8 @@ namespace my_globalplanner{
                 if(gcost[next_i] < MAX_INIT) continue;
 
                 // obstacle
-                if(costs[next_i] >= lethal_cost_) continue;
+	            int n = (int)costs[next_i];
+                if(n >= 243) continue;
 
                 // cost = gcost + hcost
                 gcost[next_i] = gcost[i] + getGcost(i, next_i);
@@ -266,6 +273,7 @@ namespace my_globalplanner{
                     std::push_heap(dequeue_.begin(), dequeue_.end(), greater1());
                 }
                 camefrom[next_i] = i; // add the index of its parent
+
             }
         }        
     }
@@ -284,6 +292,7 @@ namespace my_globalplanner{
         int goal_y = goal_i / width;
         return abs(goal_x - next_x) + abs(goal_y - goal_x); 
     }
+
     void DAstar::publishPlan(const std::vector<geometry_msgs::PoseStamped>& path){
         if(!initialized_){
             ROS_ERROR("This planner has not been initialized yet, but it is being used");
@@ -291,19 +300,17 @@ namespace my_globalplanner{
         }
 
         nav_msgs::Path gui_path;
-
         gui_path.poses.resize(path.size());
+
         gui_path.header.frame_id = frame_id_;
         gui_path.header.stamp = ros::Time::now();
 
         for(unsigned int i = 0; i < path.size(); i++){
-
             gui_path.poses[i] = path[i];
-
         }
-        plan_pub_.publish(gui_path);
 
+        plan_pub_.publish(gui_path);
     }
 
-}
 
+}
